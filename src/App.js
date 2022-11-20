a699aa3548da856519f1e8aa0d1d7ba13f8350e2
learnReactgithub <sonukushwah362@gmail.com>
2022-11-20 20:48:19 +0530

    react timer project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+let adjusinterval = undefined;
 function App() {
+  const [watch, setWatch] = useState(0);
+  const [started, setStarted] = useState(false);
+  const start = () => {
+    adjusinterval = setInterval(() => {
+      //setWatch(watch+1);
+      setWatch((x) => x + 1);
+    }, 1000);
+    setStarted(true);
+  };
+  const stop = () => {
+    clearInterval(adjusinterval);
+    setStarted(false);
+  };
+  const Reset = () => {
+    setWatch(0);
+    clearInterval(adjusinterval);
+    setStarted(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h3 className="text-center">Start Stop Watch</h3>
+        <h3 className="text-center">{watch}</h3>
+        <div className="timer">
+          <button disabled={started} onClick={start} className="start">
+            Start
+          </button>
+          <button onClick={stop} className="stop">
+            Stop
+          </button>
+          <button onClick={Reset} className="reset">
+            Reset
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
